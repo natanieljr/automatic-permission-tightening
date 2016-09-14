c9e1f5477f1f9fec5a551fcb552d9567b3867a5f
Nataniel Borges Jr <s8napere@stud.uni-saarland.de>
2016-09-14 09:39:19 +0200

    APK inliner now returns a mapping between the original APK and it's inlined version
    Fixed documentation on methods

--- apk_inliner.py
+++ apk_inliner.py
@@ -15,6 +15,11 @@ class ApkInliner(object):
     output_directory = None
 
     def set_tmp_directory(self, tmp_directory):
+        """
+        Configure the working directories for the APK inlining process
+        :param tmp_directory: Temporary directory to store files
+        :return:
+        """
         logging.debug("Configuring temporary APK inliner directory: %s", tmp_directory)
         self.tmp_directory = tmp_directory
         self.input_directory = os.path.join(self.tmp_directory, "apks")
@@ -29,7 +34,13 @@ class ApkInliner(object):
 
         assert os.path.exists(self.input_directory)
 
-    def __run_droidmate(self, directory):
+    @staticmethod
+    def __run_droidmate(directory):
+        """
+        Execute DroidMate with -inline command to inline all original APKs
+        :param directory: Directory containing the original APKs
+        :return:
+        """
         command = DROIDMATE_INLINE_APK
         command[-1] = command[-1] % directory
         logging.debug("Executing DroidMate command: %s", command)
@@ -37,15 +48,24 @@ class ApkInliner(object):
         assert signal == 0
 
     def __copy_apks_to_tmp_folder(self, apks):
+        """
+        Copy the original APKs to the temporary directory to be processed
+        :param apks: List of APKs to be copied
+        :return:
+        """
         logger.debug("Copying original APKs to temporary folder (from %s to %s)", os.path.dirname(apks[0].filename), self.input_directory)
         for apk in apks:
-            file_name = os.path.basename(apk.filename)
-            logger.debug("Copying %s", file_name)
+            file_name = apk.get_basename()
+            logger.debug("Copying to temporary: %s", file_name)
             src = apk.filename
             dst = os.path.join(self.input_directory, file_name)
             copyfile(src, dst)
 
     def __copy_inlined_apks_to_output_folder(self):
+        """
+        Copy the inlined APKs to the output directory
+        :return:
+        """
         if os.path.exists(self.output_directory):
             shutil.rmtree(self.output_directory)
         os.mkdir(self.output_directory)
@@ -54,12 +74,18 @@ class ApkInliner(object):
         logger.debug("Copying inlined APKs to output folder (from %s to %s)", self.input_directory, self.output_directory)
         for f in os.listdir(self.input_directory):
             if f.lower().endswith(".apk"):
-                logger.debug("Copying %s", f)
+                logger.debug("Copying to output %s", f)
                 src = os.path.join(self.input_directory, f)
                 dst = os.path.join(self.output_directory, f)
                 copyfile(src, dst)
 
     def process(self, apks):
+        """
+        Inline the list of APKs. This process is done by calling DroidMate internally with "-inline" argument.
+        Moreover, all original APKs are copied to the working directory, therefore preventing changes int he original
+        :param apks: List of APKs to be inlined
+        :return: Dictonary mapping each APK to an inlined APK
+        """
         assert self.output_directory is not None
 
         # Copy APKs to temporary directory
@@ -71,3 +97,17 @@ class ApkInliner(object):
 
         # Copy inlined APKs to output folder
         self.__copy_inlined_apks_to_output_folder()
+
+        # Map original APKs and inlined APKs
+        mapping = {}
+        for apk in apks:
+            file_name = apk.get_basename().replace('.apk', '-inlined.apk')
+            if os.path.exists(os.path.join(self.output_directory, file_name)):
+                mapping[apk.get_basename()] = file_name
+            else:
+                logger.warn("Unable to identify inlined version of apk %s", apk)
+
+        for k in mapping.keys():
+            print(k, mapping[k])
+
+        return mapping
